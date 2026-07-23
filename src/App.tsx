4f0a0ba5d9dfb53fbd530208e765c08769c42a29
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-23 17:11:57 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <button className="bg-orange-500 p-2 rounded-3xl text-center mx-auto">
+        let Build to go
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/auth/login.tsx
@@ -0,0 +1,9 @@
+const login = () => {
+  return (
+    <div>
+      <h1>sohan</h1>
+    </div>
+  );
+};
+
+export default login;
