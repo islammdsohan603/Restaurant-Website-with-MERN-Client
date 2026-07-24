a9076ec1a4f9a5a94b0ae34943ec2d20d9c3b22b
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-24 16:16:06 +0600

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import MainLayout from './MainLayout';
+
 import { Login } from './auth/Login';
 import Signup from './auth/Signup';
 import ForgotPassword from './auth/ForgotPassword';
 import ResetPassword from './auth/ResetPassword';
 import VerifyEmail from './auth/VerifyEmail';
+import Navbar from './components/Navbar';
 
 const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <MainLayout />,
+    element: <Navbar />,
   },
   {
     path: '/login',
